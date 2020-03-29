89353faa80ada4ff114de3ffb3206ced4849381c
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2020-03-29 02:35:32 -0700

    BROKEN init

--- src/main/java/kos/evolutionterraingenerator/EvolutionTerrainGenerator.java
+++ src/main/java/kos/evolutionterraingenerator/EvolutionTerrainGenerator.java
@@ -4,43 +4,43 @@ import kos.evolutionterraingenerator.world.EvoType;
 import kos.evolutionterraingenerator.world.biome.EvoBiomes;
 import kos.evolutionterraingenerator.world.biome.BiomeHandler;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeHills;
-import net.minecraft.world.biome.BiomeTaiga;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = EvolutionTerrainGenerator.MODID, version = EvolutionTerrainGenerator.VERSION)
+@Mod(value = EvolutionTerrainGenerator.MODID)
 public class EvolutionTerrainGenerator {
 	
 	public static final String MODID = "evolutionterraingenerator";
-	public static final String VERSION = "0.1.0";
 	
-	@Instance(MODID)
-	public static EvolutionTerrainGenerator instance;
+	public EvolutionTerrainGenerator()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+	}
 
 	//Event Loaders
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event)
+	private void setup(final FMLCommonSetupEvent event)
 	{
 		EvoType.register();
 		//NewBiomes.initBiomes();
 		MinecraftForge.EVENT_BUS.register(new BiomeHandler());
 	}
 
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event)
+	private void init(final FMLCommonSetupEvent event)
 	{
 		BiomeHandler.setBiomeTypes();
 	}
 
-	@Mod.EventHandler
-	public void init(FMLPostInitializationEvent event)
+	private void init(FMLPostInitializationEvent event)
 	{
 		EvoBiomes.init();
 	}
